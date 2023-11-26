0973c094bfb973a15b0d5d9e329b89829db7c016
Maria Stella Lopez Carvajal <102491557+marialopez9629@users.noreply.github.com>
2023-11-26 02:13:15 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -114,7 +114,7 @@ def pregunta_04():
         'mlpregressor__activation': ['relu'],  
         'mlpregressor__learning_rate': ["adaptive"],  
         'mlpregressor__momentum': [0.7,0.8,0.9],  
-        'mlpregressor__activation_learning_rate_init': [0.01,0.05,0.1],    
+        'mlpregressor__learning_rate_init': [0.01,0.05,0.1],    
     }
 
     estimator = pregunta_03()
