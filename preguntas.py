d2df026a11fdf2a8317c23ad7ac5ff7bbe9c9df6
Maria Stella Lopez Carvajal <102491557+marialopez9629@users.noreply.github.com>
2023-11-26 01:52:19 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -116,7 +116,7 @@ def pregunta_04():
         "momentum": [0.7,0.8,0.9],  
         "learning_rate_init": [0.01,0.05,0.1],  
         "max_iter": [500],  
-        "early_stopping": True,  
+        "early_stopping": [True],  
     }
 
     estimator = pregunta_03()
